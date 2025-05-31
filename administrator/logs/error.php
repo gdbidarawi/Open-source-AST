669486b36c79f0b149b302677e2c7da656f505c6
gdbidarawi <gdbidara21@gmail.com>
2025-05-31 09:36:52 +0300

    Initial commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2025-05-31 05:50:02 UTC
+#Software: Joomla! 5.3.1 Stable [ Timu ] 27-May-2025 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2025-05-31T05:50:01+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2025-05-31T05:50:20+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2025-05-31T05:50:32+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2025-05-31T05:50:41+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2025-05-31T05:51:01+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
